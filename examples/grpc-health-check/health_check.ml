eafe57ceb5a4606db7ca2881de454807f131bd89
Tim McGilchrist <timmcgil@gmail.com>
2023-11-06 07:39:34 +1100

    WIP

--- examples/grpc-health-check/health_check.ml
+++ examples/grpc-health-check/health_check.ml
@@ -1,6 +1,36 @@
 open Grpc_eio
-open Health_check_proto.Health
+open Health_check_proto.Health.Grpc.Health.V1
 open Ocaml_protoc_plugin
+
+type serving_status =
+  | Unknown
+  (* The service status is unknown. *)
+
+  | Serving
+  (* The service is currently up and serving requests. *)
+
+  | NotServing
+  (* The service is currently down and not serving requests. *)
+
+let serving_status_to_string s =
+  let open Health.Check.Response.ServingStatus in
+  match s with
+  | Unknown -> UNKNOWN
+  | Serving -> SERVING
+  | NotServing -> NOT_SERVING
+
+let check buffer =
+  let decode, encode = Service.make_service_functions Health.check in
+  let _service_name =
+    Reader.create buffer |> decode
+    |> function
+      | Ok v -> v
+      | Error e -> failwith
+                     (Printf.sprintf "Could not decode request: %s" (Result.show_error e))
+  in
+  (* TODO Lookup service health based on service_name. *)
+  let reply = Health.Check.Response.(make ~status:ServingStatus.SERVING ()) in
+  (Grpc.Status.(v OK), Some (encode reply |> Writer.contents))
 
 let add_health routes check check_streaming =
   let service = Server.Service.(
